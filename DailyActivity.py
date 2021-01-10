f2f435c368b45b73ee1df0745f9105d908c09acb
Thmtran <thmanhtung1@gmail.com>
2021-01-10 21:39:58 +0700

    Description for Daily Activity

---
+++ DailyActivity.py
@@ -0,0 +1,6 @@
+#------ This script will follow CanSlim Daily Activity -------
+#Step 1 : "Nhip Dap Thi truong"
+    # xu huong giam hay tang?
+    # cp dan dat nao dang tang gia?
+    # xu huong hien tai va cach ung xu ?
+#Step 2 : "Nhin lai cac co phieu dang so huu xem nhu the nao"
